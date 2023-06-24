ecc9d4f449aef62e1af39d65f9645de4859902f4
Shahabhct <79043647+Shahabhct@users.noreply.github.com>
2023-06-24 13:28:19 +0400

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("count") var count = 0
+    @AppStorage("remaining") var cookiesRemaining = 40
+    @AppStorage("message") private var message = ""
+    
+    var body: some View {
+        VStack {
+            Text("\(count) Cookies eaten!")
+            Text("\(cookiesRemaining - count) Cookies remaining")
+            Button {
+                count += 1
+                if count >= 5 {
+                    message = "You are eating too much"
+                }
+                if count >= 10 {
+                    message = "hey stop eating"
+                }
+                if count >= 15 {
+                    message = "Oh noooooo"
+                }
+                
+            }label: {
+                Text("eat a cookie")
+                    .padding()
+                    .font(.system(size: 40))
+                    .background(.blue)
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+            }
+            //.buttonStyle(.borderedProminent)
+            
+            .shadow(color: .black.opacity(0.9), radius: 10, x: 0.0, y: 0.0)
+            
+           Text(message)
+        }
+    }
+}
